56039395b7b85e296ed8b7b4a9d6ebb57b3d71ec
Teemu Kallio <Teemu.Kallio3@metropolia.fi>
2024-04-08 07:30:09 +0300

    user update method

--- restaurant app/profile/profile.js
+++ restaurant app/profile/profile.js
@@ -1,1 +1,24 @@
 import {updateUser, getUserToken, uploadAvatar, checkUsernameAvailability} from "../mainApp/variables.js";
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  const form = document.getElementById('updateUserForm');
+
+  form.addEventListener('submit', async function (e) {
+    e.preventDefault();
+
+    const accessToken = getUserToken();
+
+    const formData = new FormData(form);
+    const userData = {};
+    formData.forEach((value, key) => {
+      userData[key] = value;
+    });
+
+    try {
+      const responseData = await updateUser(userData, accessToken);
+      console.log('Update successful', responseData);
+    } catch (error) {
+      console.error('Update failed', error);
+    }
+  });
+});
