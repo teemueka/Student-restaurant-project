945397a1724d27fc55decd5b269bb181e42bc36f
Teemu Kallio <Teemu.Kallio3@metropolia.fi>
2024-04-10 11:49:26 +0300

    dynamic login and registration

--- restaurant app/login/login.js
+++ restaurant app/login/login.js
@@ -92,3 +92,55 @@ const handleRegistration = async (userData) => {
     }
   }
 };
+const registration = () => {
+  form.innerHTML = `
+    <h1>Registration</h1>
+    <div class="input-control">
+      <label for="username">Username</label>
+      <input id="username" name="username" type="text" required />
+      <div class="error" id="username-error"></div>
+    </div>
+    <div class="input-control">
+      <label for="email">Email</label>
+      <input id="email" name="email" type="text" required />
+      <div class="error" id="email-error"></div>
+    </div>
+    <div class="input-control">
+      <label for="password">Password</label>
+      <input id="password" name="password" type="password" required />
+      <div class="error" id="password-error"></div>
+    </div>
+    <div class="alreadyUser">
+      <p>Already have an account?</p>
+      <a href="#" id="switchToLogin">Sign in</a>
+    </div>
+    <button type="submit" >Register</button>`;
+
+  document.getElementById('switchToLogin').addEventListener('click', login);
+
+}
+
+const login = () => {
+  form.innerHTML = `
+    <h1>Login</h1>
+    <div class="input-control">
+      <label for="username">Username</label>
+      <input id="username" name="username" type="text" required />
+      <div class="error" id="username-error"></div>
+    </div>
+    <div class="input-control">
+      <label for="password">Password</label>
+      <input id="password" name="password" type="password" required />
+      <div class="error" id="password-error"></div>
+    </div>
+    <div class="alreadyUser">
+      <p>Make an account</p>
+      <a href="#" id="switchToRegistration">Sign up</a>
+    </div>
+    <button type="submit">Log in</button>`;
+
+  document.getElementById('switchToRegistration').addEventListener('click', registration);
+
+}
+
+registration();
